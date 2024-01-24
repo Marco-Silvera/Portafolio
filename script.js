415ab542cdca37c06296ff966cf01b83e6da17a5
Marco-Silvera <marksilver_1_0@hotmail.com>
2024-01-23 23:10:38 -0500

    Actualización de Portafolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Deslizamiento suave
+
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
