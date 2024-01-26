62ec1e1da33fab4c13b73b6f9f5912f12d4be2d7
Marco-Silvera <marksilver_1_0@hotmail.com>
2024-01-26 13:41:51 -0500

    Actualización Portafolio

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
+
+
+// Menu responsive
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex';
+    toggleMenuButtonVisibility(false);
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none';
+    toggleMenuButtonVisibility(true);
+}
+
+function toggleSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    const currentDisplay = window.getComputedStyle(sidebar).getPropertyValue('display');
+
+    if (currentDisplay === 'none') {
+        sidebar.style.display = 'flex';
+        toggleMenuButtonVisibility(false);
+    } else {
+        sidebar.style.display = 'none';
+        toggleMenuButtonVisibility(true);
+    }
+}
+
+function toggleMenuButtonVisibility(show) {
+    const menuButton = document.getElementById("menuButton");
+    menuButton.style.display = show ? 'block' : 'none';
+}
