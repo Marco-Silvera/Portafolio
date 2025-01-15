a09ea6ff1ee97eb2ea7aa7387163674622eff7e6
Marco Silvera <marksilver_1_0@hotmail.com>
2025-01-14 22:43:36 -0500

    Modificando tamaño del footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,11 +4,11 @@ export default function Footer() {
 
     return (
         <div
-            className='relative h-[300px] md:h-[500px] w-full'
+            className='relative h-[300px] lg:h-[400px] w-full'
             style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
         >
-            <div className='relative h-[calc(100vh+300px)] md:h-[calc(100vh+500px)] -top-[100vh]'>
-                <div className='h-[300px] md:h-[500px] sticky top-[calc(100vh-300px)] md:top-[calc(100vh-500px)]'>
+            <div className='relative h-[calc(100vh+300px)] lg:h-[calc(100vh+400px)] -top-[100vh]'>
+                <div className='h-[300px] lg:h-[400px] sticky top-[calc(100vh-300px)] lg:top-[calc(100vh-400px)]'>
                     <Content />
                 </div>
             </div>
